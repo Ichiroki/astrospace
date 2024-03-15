ab3fdc29cf008d5fd5317d5fc0c558b2ed444705
Ichiroki <fahrezirizqiawan12649@gmail.com>
2024-03-15 07:29:29 +0700

    fixing banner section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,26 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        'flying-astronaut-responsive': {
+          '0%': { top: '-1rem' },
+          '25%': { top: '-2rem'},
+          '50%': { top: '-3rem' },
+          '100%': { top: '-4rem' }
+        },
+        'flying-astronaut': {
+          '0%': { 'margin-bottom': 0 },
+          '25%': { 'margin-bottom': '1.5rem' },
+          '50%': { 'margin-bottom': '3rem' },
+          '100%': { 'margin-bottom': '6rem' }
+        }
+      },
+      animation: {
+        'flying-astronaut-responsive': 'flying-astronaut-responsive 5s ease-in-out infinite alternate',
+        'flying-astronaut': 'flying-astronaut 5s ease-in-out infinite alternate'
+      },
+    },
   },
   plugins: [],
 };
